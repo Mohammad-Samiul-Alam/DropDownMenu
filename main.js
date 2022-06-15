de4475732c928de6d5eeae84914dcf7f75a16a31
samiulrabbi <102017413+samiulrabbi@users.noreply.github.com>
2022-06-15 18:03:18 +0600

    second

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // toggle sub menus
+    $(".sub-btn").click(function(){
+         $(this).next(".sub-menu").slideToggle();
+    })
+    // toggle more menus
+    $(".more-btn").click(function(){
+        $(this).next(".more-menu").slideToggle();
+    })
+    // toggler btn (open and close)
+    $(".toggler-btn").click(function(){
+        $(this).toggleClass("active");
+    })
+    // toggler btn menu(hide and show)
+    $(".toggler-btn").click(function(){
+        $(".menu").toggleClass("active");
+    })
+})
