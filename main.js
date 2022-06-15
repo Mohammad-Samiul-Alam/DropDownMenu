2338df736b383a390f16d6c6043116c8bcabd624
samiulrabbi <102017413+samiulrabbi@users.noreply.github.com>
2022-06-15 18:44:44 +0600

    third

--- main.js
+++ main.js
@@ -15,4 +15,7 @@ $(document).ready(function(){
     $(".toggler-btn").click(function(){
         $(".menu").toggleClass("active");
     })
+    $(".more-btn").click(function(){
+        $(this).toggleClass("active")
+    })
 })
